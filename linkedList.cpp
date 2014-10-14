f0380ff34085a71ababc284d559207ce33faa392
Max Li <maxhongmingli@gmail.com>
2014-10-14 19:55:53 -0400

    Added linked lists, not implemented yet

---
+++ linkedList.cpp
@@ -0,0 +1,89 @@
+#include "linkedList.h"
+#include "Robot.h"
+#include "Shopper.h"
+
+EventNode::EventNode()
+  :time(0), robID(NULL), shopperID(NULL), eType(0)
+{}
+
+EventNode::EventNode(int newTime, Robot *robIn, int type)
+  :time(newTime), robID(robIn),shopperID(NULL), eType(type)
+{}
+
+EventNode::EventNode(int newTime, Shopper *shopIn, int type)
+  :time(newTime), robID(NULL), shopperID(shopIn), eType(type)
+{}
+
+int EventNode::getTime(){
+  return time;
+}
+
+Robot *EventNode::getRobotID(){
+  return robotID;
+}
+
+Shopper *EventNode::getShopperID(){
+  return shopperID;
+}
+
+int get_eType(){
+  return eType;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+// default constructor  
+ EventList::EventList() 
+   : firstPtr( 0 ), lastPtr( 0 ) 
+{ 
+  // empty body 
+} // end List constructor 
+
+// destructor 
+ EventList::~EventList() 
+{ 
+  if ( !isEmpty() ) // List is not empty 
+    { 
+      cout << "Destroying nodes ...\n"; 
+
+      EventNode *currentPtr = firstPtr; 
+      EventNode *tempPtr; 
+
+      while ( currentPtr != 0 ) // delete remaining nodes 
+	{ 
+	  tempPtr = currentPtr; 
+	  //cout << tempPtr->data << '\n'; 
+	  currentPtr = currentPtr->nextPtr; 
+	  delete tempPtr; 
+	} // end while 
+    } // end if 
+
+  cout << "All nodes destroyed\n\n"; 
+} // end List destructor 
+
+void EventList::addNode(EventNode *newNode){
+  EventNode *cur = getFirstPtr();
+  EventNode *prev = NULL;
+  EventNode *last = getLastPtr();
+
+  while(cur != NULL && cur->getTime() < newNode->getTime()){
+    prev = cur;
+    cur = cur->getNext();
+  }
+  if(cur == NULL){
+    prev->setNext(newNode);
+  }
+  else{
+    prev->setNext(newNode);
+    newNode->setNext(cur);
+  }
+}
+
+void EventList::addNewEvent(int time, Robot *newR, int type){
+  EventNode *newEvent = new EventNode(time, newR, type);
+  addNode(newEvent);
+}
+
+void EventList::addNewEvent(int time, Shopper *newShop, int type){
+  EventNode *newEvent = new EventNode(time, newShop, type);
+  addNode(newEvent);
+}
